1f7ba5b8ad046bfbd21ab4f86a83a7a7d45009a4
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-11-20 23:35:59 +0100

    Made kluster generically usable

--- cluster.h
+++ cluster.h
@@ -1,21 +1,118 @@
 #include <vector>
+#include <iostream>
 
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#include "data.h"
+#include "utils.h"
 #include "storage.h"
 
 
+template <class T>
 class Kluster {
 public:
     Kluster(int k);
 
-    void init(std::vector<Gene> genes);
-    std::vector< std::vector<Gene> > run();
+    void init(std::vector<T>);
+    std::vector< std::vector<T> > run();
+
+    std::map<std::string, double> (*get_cluster_mean)(std::vector<T>);
+    int (*get_clostest_cluster_index)(T, std::vector< std::map<std::string, double> >);
 private:
     int k;
-    std::vector< std::vector<Gene> > clusters;
-    std::vector<Gene> originals;
+    std::vector< std::vector<T> > clusters;
+    std::vector<T> originals;
 
-    std::vector< std::vector<Gene> > generate_clusters();
-
-    std::map<std::string, double> get_cluster_mean(std::vector<Gene> cluster);
-    int get_clostest_cluster_index(Gene gene, std::vector< std::map<std::string, double> > means);
+    std::vector< std::vector<T> > generate_clusters();
 };
+
+
+/*
+ * Function Implementations
+ */
+
+template <class T>
+Kluster<T>::Kluster(int k) {
+    this->k = k;
+
+    std::srand(std::time(NULL));
+}
+
+template <class T>
+std::vector< std::vector<T> > Kluster<T>::generate_clusters() {
+    std::vector< std::vector<T> > tmp;
+
+    for(std::size_t i = 0 ; i < k ; i++) {
+        tmp.push_back(std::vector<T>());
+    }
+
+    return tmp;
+}
+
+template <class T>
+void Kluster<T>::init(std::vector<T> elements) {
+    originals = elements;
+
+    // create empty clusters
+    clusters = generate_clusters();
+
+    // fill 'em
+    for(std::size_t i = 0 ; i < elements.size() ; i++) {
+        int ind = Utils::rand_int(0, k-1);
+        clusters[ind].push_back(elements[i]);
+    }
+}
+
+template <class T>
+std::vector< std::vector<T> > Kluster<T>::run() {
+    std::vector<int> old_sizes;
+    for(std::size_t i = 0 ; i < k ; i++) {
+        old_sizes.push_back(-1);
+    }
+
+    int runs = 0;
+    while(true) {
+        // info
+        for(std::size_t i = 0 ; i < k ; i++) {
+            std::cout << "Elements in cluster #" << i << ": " << clusters[i].size() << std::endl;
+        }
+        std::cout << std::endl;
+
+        // compute current cluster means
+        std::vector< std::map<std::string, double> > means;
+        for(std::size_t i = 0 ; i < k ; i++) {
+            means.push_back(get_cluster_mean(clusters[i]));
+        }
+
+        // generate new sets and put elements in there
+        std::vector< std::vector<T> > tmp = generate_clusters();
+        for(std::size_t i = 0 ; i < originals.size() ; i++) {
+            T element = originals[i];
+            int ind = get_clostest_cluster_index(element, means);;
+
+            tmp[ind].push_back(element);
+        }
+
+        // check if we should stop
+        std::vector<int> new_sizes; // tmp workaround
+        for(std::size_t i = 0 ; i < k ; i++) {
+            new_sizes.push_back(clusters[i].size());
+        }
+
+        if(old_sizes == new_sizes) {
+           break;
+        } else {
+            clusters = tmp;
+            old_sizes = new_sizes;
+            
+            // remove empty entries and update k
+            // ...
+        }
+
+        runs++;
+    }
+
+    return clusters;
+}
--- main.cpp
+++ main.cpp
@@ -8,13 +8,58 @@
 #include "cluster.h"
 
 
+std::map<std::string, double> get_cluster_mean(std::vector<Gene> cluster) {
+    std::map<std::string, double> mean;
+
+    for(std::size_t i = 0 ; i < cluster.size() ; i++) {
+        Gene g = cluster[i];
+
+        for(std::map<std::string, double>::const_iterator it = g.codon_freqs.begin() ; it != g.codon_freqs.end() ; it++) {
+            mean[it->first] += it->second;
+        }
+    }
+
+    for(std::map<std::string, double>::const_iterator it = mean.begin() ; it != mean.end() ; it++) {
+        mean[it->first] /= cluster.size();
+    }
+
+    return mean;
+}
+
+int get_clostest_cluster_index(Gene element, std::vector< std::map<std::string, double> > means) {
+    int closest = 0;
+    double old_dist = 1000000000;
+
+    for(std::size_t i = 0 ; i < means.size() ; i++) {
+        double dist = 0;
+
+        std::vector<double> res;
+        for(std::size_t j = 0 ; j < all_codons.size() ; j++) {
+            std::string key = all_codons[j];
+            res.push_back(std::abs(means[i][key] - element.codon_freqs[key]));
+        }
+
+        for(std::size_t j = 0 ; j < res.size() ; j++) {
+            dist += res[j];
+        }
+        //dist /= res.size();
+
+        if(dist < old_dist) {
+            closest = i;
+            old_dist = dist;
+        }
+    }
+
+    return closest;
+}
+
 int main(int argc, char **argv) {
     std::vector<Gene> genes;
 
     /*
      * Parse data
      */
-	std::ifstream fin("data/dicty_primary_cds.fasta");
+	std::ifstream fin("data/simple.fasta");
 	fasta ele;
 
     while(!fin.eof()) {
@@ -35,10 +80,14 @@ int main(int argc, char **argv) {
     /*
      * Some k-means clustering
      */
-    Kluster clus(6);
+    Kluster<Gene> clus(6);
+
+    clus.get_cluster_mean = &get_cluster_mean;
+    clus.get_clostest_cluster_index = &get_clostest_cluster_index;
+
     clus.init(genes);
     std::vector< std::vector<Gene> > res = clus.run();
-    Analyzer::cluster_stats(res);
+    //Analyzer::cluster_stats(res);
 
 	return 0;
 }
